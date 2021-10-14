53577c3d7342c37b782cddd87eb38dd965ab0130
Vitoudi <69259931+Vitoudi@users.noreply.github.com>
2021-10-14 10:02:58 -0300

    change db log

--- server/dist/data/db.js
+++ server/dist/data/db.js
@@ -5,13 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 var mysql2_1 = __importDefault(require("mysql2"));
-var connection = mysql2_1.default.createConnection({
+var connectionObj = {
     host: process.env.DB_host,
     password: process.env.DB_password,
     user: process.env.DB_user,
     database: process.env.DB_database,
     port: process.env.DB_port ? +process.env.DB_port : 3306,
-});
-console.log("CONNECTION: ", connection);
+};
+var connection = mysql2_1.default.createConnection(connectionObj);
+console.log("CONNECTION: ", connectionObj);
 exports.db = connection.promise();
 exports.db.connect;
--- server/src/data/db.ts
+++ server/src/data/db.ts
@@ -1,14 +1,16 @@
 import mySql from "mysql2"
 
-const connection = mySql.createConnection({
+const connectionObj = {
   host: process.env.DB_host,
   password: process.env.DB_password,
   user: process.env.DB_user,
   database: process.env.DB_database,
   port: process.env.DB_port ? +process.env.DB_port : 3306,
-});
+}
 
-console.log("CONNECTION: ", connection);
+const connection = mySql.createConnection(connectionObj);
+
+console.log("CONNECTION: ", connectionObj);
 
 
 export const db = connection.promise();
