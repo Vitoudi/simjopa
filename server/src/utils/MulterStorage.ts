72e5fadc616fb057186007d03c85c1a7d265ba63
Vitoudi <69259931+Vitoudi@users.noreply.github.com>
2021-10-25 14:55:42 -0300

    remove Date.now() on file name

--- server/src/utils/MulterStorage.ts
+++ server/src/utils/MulterStorage.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 
 function getUniqueFileName(originalFileName: string) {
-  return `${Date.now()}-${originalFileName.replace(/ /g, '')}`;
+  return `${originalFileName.replace(/ /g, '')}`;
 }
 
 export const multerUserStorage = multer.diskStorage({
